cc53430a4be986bd86fa33f83a6ce0a5603a9ec1
spaderdabomb <jpdodson42@gmail.com>
2025-04-29 22:44:26 -0700

    build fix 1

--- components/about.tsx
+++ components/about.tsx
@@ -8,6 +8,15 @@ import { useSectionInView } from "@/lib/hooks";
 export default function About() {
   const { ref } = useSectionInView("About");
 
+  const aboutText = `As a Santa Barbara local and fellow pet enthusiast, 
+      I founded Santa Barbara Sitters to offer the kind of trustworthy 
+      and loving care I'd want for my own furry friends. 
+      I understand how important it is to find someone reliable and compassionate when you can\'t be there. 
+      That's why I'm personally committed to ensuring your pets feel safe, happy, and comfortable in their own home. 
+      I offer personalized pet sitting services tailored to your pet's unique needs, 
+      giving you peace of mind knowing they're in caring hands right here in our beautiful Santa Barbara community.`;
+
+
   return (
     <motion.section
       ref={ref}
@@ -19,13 +28,7 @@ export default function About() {
     >
       <SectionHeading>About Me</SectionHeading>
       <p className="mb-3">
-      As a Santa Barbara local and fellow pet enthusiast, 
-      I founded Santa Barbara Sitters to offer the kind of trustworthy 
-      and loving care I'd want for my own furry friends. 
-      I understand how important it is to find someone reliable and compassionate when you can't be there. 
-      That's why I'm personally committed to ensuring your pets feel safe, happy, and comfortable in their own home. 
-      I offer personalized pet sitting services tailored to your pet's unique needs, 
-      giving you peace of mind knowing they're in caring hands right here in our beautiful Santa Barbara community.
+        {aboutText}
       </p>
     </motion.section>
   );
